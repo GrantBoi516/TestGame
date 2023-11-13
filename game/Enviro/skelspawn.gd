4f86023192693c7360794e37e413b6e9956487f6
GrantBoi516 <87438426+GrantBoi516@users.noreply.github.com>
2023-11-13 09:20:20 -0500

    Add files via upload

---
+++ game/Enviro/skelspawn.gd
@@ -0,0 +1,11 @@
+extends Area2D
+@onready var skeleton = load("res://Enemies/skeleton/skeleton.tscn")
+
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.has_method("movement"):
+		var instancedSkel = skeleton.instantiate()
+		get_parent().add_child(instancedSkel)
+		instancedSkel.position.x = position.x
+		instancedSkel.position.y = position.y - 100
