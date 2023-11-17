bd61a936db309b1076bd3101ced40ff27a280a8a
GrantBoi516 <87438426+GrantBoi516@users.noreply.github.com>
2023-11-16 20:26:51 -0500

    Add files via upload

---
+++ game/Player/weapon.gd
@@ -0,0 +1,22 @@
+extends Area2D
+@onready var anim = $WeaponAnim
+@export var damage := 1
+
+
+
+
+func _physics_process(_delta):
+	look_at(get_global_mouse_position())#points sword towards mouse
+
+
+
+
+func attack():#attack method referenced in parent script
+	anim.play("swordSwing")
+
+
+
+
+func _on_body_entered(body: Node) -> void:#detects when it collides with and enemy and deals damage
+	if body.has_method("get_hit"):
+		body.get_hit(damage)
