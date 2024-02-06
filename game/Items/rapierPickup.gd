d2c53b3a88d335861aeafa3cfc844e86406b6397
GrantBoi516 <87438426+GrantBoi516@users.noreply.github.com>
2024-02-05 20:00:15 -0500

    Delete game directory

--- game/Items/rapierPickup.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(body: Node) -> void:
-	if body.has_method("rapier_change"):
-		body.rapier_change()
-		queue_free()
--- game/Menus/W.gd
+++
@@ -1,6 +0,0 @@
-extends VFlowContainer
-
-@onready var player = get_parent().get_parent().get_node("CharacterBody2D")
-
-func _process(_delta):
-	$Label.text = "weapon: " + player.weapon_val
